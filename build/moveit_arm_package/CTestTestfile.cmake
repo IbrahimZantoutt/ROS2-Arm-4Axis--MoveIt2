9cc91f1343e5fe70d6cc03d68075b553df149997
Ibrahim Zantout <kingofdeath3636@gmail.com>
2026-05-27 22:00:22 +0300

    first commit (4 axis arm urdf and config)

---
+++ build/moveit_arm_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ibrahim/MoveItArm/src/moveit_arm_package
+# Build directory: /home/ibrahim/MoveItArm/build/moveit_arm_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
